7d58be4add379ca92d95e965dcdd8e722620022c
jannunzi <jannunzi@gmail.com>
2020-03-19 20:05:46 -0400

    added question service

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express')
 var app = express()
 
+app.use(function (req,res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 const quizController = require('./controllers/quiz.controller.server')
 quizController(app)
 
